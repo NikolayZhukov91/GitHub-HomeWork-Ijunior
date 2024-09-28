a488dd106d0bbfd445cd596ecb87543b93d85044
NikolayZhukov91 <nikolay.zhukov.nz@gmail.com>
2024-09-28 08:52:41 +0300

    Variables
    HomeWork №1

---
+++ variables.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    int ammoGun = 340;
+    int health  = 100;
+    float damageGun = 12,4f;
+    float armor = 4,5f;
+    char team1 = 'A';
+    char team2 ='B';
+    string hello = "Hello, Nikolay!";
+    string startGame = "Go!";
+    string gameOver = "Game Over!"
+    bool isStartGame = true;
+    bool isPauseGame = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
