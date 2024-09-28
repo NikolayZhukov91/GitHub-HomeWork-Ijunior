252292f5771ff7eea91a4255cd7a7ca72a990a42
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-09-28 10:21:40 +0300

    Add files via upload

---
+++ Variables.v2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class variables
+{
+    int ammoOfGun = 340;
+    int healthOfPlayer = 100;
+    float damageOfGun = 12,4f;
+    float armorOfPlayer = 4,5f;
+    char team1 = 'A';
+    char team2 = 'B';
+    string helloMessage = "Hello, Nikolay!";
+    string startGameMessage = "Go!";
+    string gameOverMessage = "Game Over!"
+    bool isStartGame = true;
+    bool isPauseGame = false;
+}
