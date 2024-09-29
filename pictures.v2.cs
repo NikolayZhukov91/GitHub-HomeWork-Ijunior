b36f89ed26105514869f7aa3d099ba9ecfce801f
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-09-29 18:50:10 +0300

    Add files via upload

---
+++ pictures.v2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacityImages = 3;
+            int ImagesAlbum = 52;
+            int fulldRows;
+            int picturesMeasure;
+            int fullRows = ImagesAlbum / capacityImages;
+            picturesMeasure = ImagesAlbum % capacityImages;
+
+            Console.WriteLine($"Всего заполенных рядов можно вывести: {fullRows}, картинок сверх меры: {picturesMeasure}");
+            Console.ReadKey();
+        }
+    }
+}
