b1ee7e1bf9eb5799618489dc1c8ad77150f8a5a0
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-09-29 18:33:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacityOfNumberOfImages = 3;
+            int numberOfimagesInUserAlbum = 52;
+            int fullFilledRows;
+            int picturesBeyondMeasure;
+            fullFilledRows = numberOfimagesInUserAlbum / capacityOfNumberOfImages;
+            picturesBeyondMeasure = numberOfimagesInUserAlbum % capacityOfNumberOfImages;
+
+            Console.WriteLine($"Всего заполенных рядов можно вывести: {fullFilledRows}, картинок сверх меры: {picturesBeyondMeasure}");
+            Console.ReadKey();
+        }
+    }
+}
