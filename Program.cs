20a59c01c87721eb03c7136965c123052d222633
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-09-29 18:01:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfUser;
+            string ageOfUser;
+            string zodiacOfUser;
+            string jobOfUser;
+
+            Console.WriteLine("Привет! как тебя зовут?");
+            nameOfUser = Console.ReadLine();
+            Console.WriteLine("Сколько тебе лет?");
+            ageOfUser = Console.ReadLine();
+            Console.WriteLine("Какой твой знак зодиака?");
+            zodiacOfUser = Console.ReadLine();
+            Console.WriteLine("Кем ты работаешь?");
+            jobOfUser = Console.ReadLine();
+
+
+            Console.WriteLine($"Вас зовут {nameOfUser}, вам {ageOfUser}, вы {zodiacOfUser} и работаете {jobOfUser}");
+            Console.ReadKey();
+        }
+    }
+}
