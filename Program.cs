1cc2409894edd942b6d1a4041e6cdda3202b37c3
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-09-30 09:24:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace crystalShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerGold;
+            int playerCristals;
+            int costOfCrystals = 10;
+            int numberOfCrystals;
+            Console.WriteLine($"Сколько у вас золота?");
+            playerGold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"1 кристалл стоит: {costOfCrystals} золота.");
+            Console.WriteLine("Сколько кристаллов вы хотите купить?");
+            numberOfCrystals = Convert.ToInt32(Console.ReadLine());
+            playerGold = playerGold - numberOfCrystals*costOfCrystals;
+            playerCristals = numberOfCrystals;
+            Console.WriteLine($"У вас: {playerGold} золота и {playerCristals} красталлов");
+            Console.ReadKey();
+        }
+    }
+}
