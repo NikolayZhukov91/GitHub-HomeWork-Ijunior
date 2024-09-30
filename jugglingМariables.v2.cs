223302538edac662251f69500d86a7b0e9d13cac
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-09-30 08:11:32 +0300

    Add files via upload

---
+++ jugglingМariables.v2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace jugglingМariables.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Zhukov";
+            string secondName = "Nikolay";
+            string support;
+            Console.WriteLine($"Первая переменная: {firstName}, вторая переменная: {secondName}.");
+            support = firstName;
+            firstName = secondName;
+            secondName = support;
+            Console.WriteLine($"Первая переменная: {firstName}, вторая переменная: {secondName}.");
+            Console.ReadKey();
+        }
+    }
+}
