c38709e7466ffba94f39698ccbddf0e803503692
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-09-30 10:36:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyclinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeReception = 10;
+            int numberOfPatients;
+            int minutsInHour = 60;
+            int waitingTimeHours;
+            int waitingTimeMinutes;
+            Console.WriteLine("Привет добро пожаловать выздоравливать!");
+            Console.WriteLine("Введите количество пациентов в очереди:");
+            numberOfPatients = Convert.ToInt32(Console.ReadLine());
+            waitingTimeHours = (numberOfPatients * timeReception) / minutsInHour;
+            waitingTimeMinutes = (numberOfPatients* timeReception) % minutsInHour;
+            Console.WriteLine($"Ваше время ожидания: {waitingTimeHours} часов и {waitingTimeMinutes} минут");
+            Console.ReadKey();
+
+        }
+    }
+}
