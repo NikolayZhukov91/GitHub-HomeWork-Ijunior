cb9886796dc39845915ddd53a1468df8125c31a0
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-01 16:18:12 +0300

    Update exitControl.v2.cs

--- exitControl.v2.cs
+++ exitControl.v2.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExitControl
 {
@@ -15,7 +11,7 @@ namespace ExitControl
 
             Console.WriteLine($"Программа будет выполняться пока вы не введете команду {codeWord}.");
 
-            while ((message = Console.ReadLine()) != codeWord)
+            while (message != codeWord)
             {
                 Console.WriteLine("Введите команду:");
                 message = Console.ReadLine();
