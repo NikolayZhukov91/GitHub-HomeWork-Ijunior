35469dd7e837b956686ec6665f28baa4ea1ea73e
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-01 15:44:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastering_cycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberMessage;
+            string message;
+
+            Console.WriteLine("Введите ваше сообщение:");
+            message = Console.ReadLine();
+            Console.WriteLine("Сколько раз повторить сообщение?");
+            numberMessage = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i<numberMessage; i++)
+            {
+                Console.WriteLine(message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
