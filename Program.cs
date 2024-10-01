bc67af3531dc3f25383b4f3d2feb83e2fc377456
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-01 10:39:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.WriteLine("Введите ва возраст:");
+
+            age = Convert.ToInt32(Console.ReadLine());
+
+            while (age-- > 0)
+            {
+                if (age == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine(age);
+                
+                if (age == 9)
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
