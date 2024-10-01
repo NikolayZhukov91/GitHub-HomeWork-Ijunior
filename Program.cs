d37c32617abd9a21fb6abf589b939ffc9dc89586
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-01 14:26:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value;
+
+            while (true)
+            {
+                value = random.Next(0,10);
+                Console.WriteLine(value);
+                Console.ReadKey();
+            }
+        }
+    }
+}
