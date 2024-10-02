5d1487bbeafcf1006d7b196d049e94a5b641fa12
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-02 14:31:28 +0300

    Update ConsoleMenu.cs

--- ConsoleMenu.cs
+++ ConsoleMenu.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleMenu
 {
@@ -54,7 +50,6 @@ namespace ConsoleMenu
                         Console.WriteLine("Такой команды нет.");
                         break;
                 }
-
             }
         }
     }
