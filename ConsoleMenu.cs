3b8b9f90ed8e433726921fe8d25007e2c0f45c6a
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-02 14:30:52 +0300

    Add files via upload

---
+++ ConsoleMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string CommandShowText = "1";
+            const string CommandClearConsole = "2";
+            const string CommandRandom  = "3";
+            const string CommandExit = "4";
+
+            bool isWork = true;
+            string userInput;
+            int minValueRandom = 0;
+            int maxValueRandom = 100;
+
+            Random random = new Random();
+            int randomNumber;
+
+            while (isWork)
+            {
+                Console.WriteLine("Привет! Возможные команды:");
+                Console.WriteLine("Показать 'Hello, World!': команда - '1'");
+                Console.WriteLine("Очистить консоль: команда - '2'");
+                Console.WriteLine("Вывести случайное число: команда - '3'");
+                Console.WriteLine("Выйти из программы: команда - '4'");
+
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case CommandShowText:
+                        Console.WriteLine("Hello, World!");
+                        break;
+                    case CommandClearConsole:
+                        Console.Clear();
+                        break;
+                    case CommandRandom:
+                        randomNumber = random.Next(minValueRandom, maxValueRandom);
+                        Console.WriteLine(randomNumber);
+                        break;
+                    case CommandExit:
+                        isWork = false;
+                        Console.WriteLine("Выход из программы");
+                        break;
+                    default:
+                        Console.WriteLine("Такой команды нет.");
+                        break;
+                }
+
+            }
+        }
+    }
+}
