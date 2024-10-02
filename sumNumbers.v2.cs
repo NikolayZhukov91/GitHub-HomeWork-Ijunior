d1b21ad2a443601db05b9b17d4cc83e8e3899606
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-02 13:54:51 +0300

    Add files via upload

---
+++ sumNumbers.v2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_sum_of_the_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumNumbers = 0;
+            int minValueRandom = 0;
+            int maxValueRandom = 100;
+            int numberFirstCondition = 3;
+            int numberSecondCondition = 5;
+            Random random = new Random();
+            int number = random.Next(minValueRandom, maxValueRandom);
+            
+
+            Console.WriteLine($"{number} - рандом");
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % numberFirstCondition == 0 || i % numberSecondCondition == 0)
+                {
+                    sumNumbers += i;
+                    Console.WriteLine($"{i} - i");
+                    Console.WriteLine($"{sumNumbers} - сумма чисел");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
