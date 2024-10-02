9886226f42676fcd828eaa46d36c6838715e8fae
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-02 13:43:22 +0300

    Add files via upload

---
+++ sequence.v2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sequence.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 5;
+            int maxValue = 789;
+            int step = 7;
+
+            Console.WriteLine(startNumber);
+
+            for (int i = startNumber; i <= maxValue; i++)
+            {
+                if (i % step == 0)
+                {                    
+                    startNumber += step;
+                    Console.WriteLine(startNumber);
+                }
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
