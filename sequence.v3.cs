0a5adb454014cf4051e1db37ad2ef4b18240f9db
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-02 15:04:49 +0300

    Add files via upload

---
+++ sequence.v3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace sequence.v3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialNumber = 5;
+            int maxValue = 789;
+            int step = 7;
+
+            Console.WriteLine(initialNumber);
+
+            for (int i = initialNumber; i <= maxValue; i += step)
+            {
+                Console.WriteLine(initialNumber);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
