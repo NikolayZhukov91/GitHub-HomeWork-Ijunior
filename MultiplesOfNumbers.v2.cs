654362c92e41276f52f2ea8e23c5872d7935c209
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-03 12:08:33 +0300

    Add files via upload

---
+++ MultiplesOfNumbers.v2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerBound = 10;
+            int upperBound = 25;
+            int conditionalMinimumNumber = 50;
+            int conditionalMaximumNumber = 150;
+            int multiplesOfNumbers = 0;
+            
+            Random random = new Random();
+            int number = random.Next(lowerBound, upperBound);
+
+            for (int i = 0; i <= conditionalMaximumNumber; i += number)
+            {
+                if (i >= conditionalMinimumNumber && i <= conditionalMaximumNumber) 
+                {
+                    multiplesOfNumbers++;
+                    Console.WriteLine(multiplesOfNumbers);
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine($"Количество чисел от {conditionalMinimumNumber} до {conditionalMaximumNumber}, которые кратны {number} равно {multiplesOfNumbers}.");
+            Console.ReadKey();
+        }
+    }
+}
