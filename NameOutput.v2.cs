02895f465f6f600c278181432d75561a07a06a4a
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-03 09:49:29 +0300

    Add files via upload

---
+++ NameOutput.v2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char userChar;
+            string userName;
+            string middleLine;
+            string frame = "";
+
+            Console.WriteLine("Введите символ:");
+            userChar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введите ваше Имя:");
+            userName = Console.ReadLine();
+
+            middleLine = userChar + userName + userChar;
+
+            for (int i = 0; i < (middleLine.Length); i++)
+            {
+                frame += userChar;
+            }
+
+            Console.WriteLine(frame);
+            Console.WriteLine(userChar + userName + userChar);
+            Console.WriteLine(frame);
+            Console.ReadKey();
+
+        }
+    }
+}
