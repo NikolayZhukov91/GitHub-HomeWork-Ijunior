ade82edffa5efd038a7a70f287d888355d3bf5ec
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-03 09:41:12 +0300

    Add files via upload

---
+++ passworsTriesCount.v2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passworsTriesCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int triesCount = 3;
+            string password = "123456";
+            string userInput;
+            int additionalAttempt = 1;
+
+            for (int i = 0; i < triesCount; i++)
+            {
+                Console.WriteLine("Введите пароль:");
+                userInput = Console.ReadLine();
+                if (userInput == password)
+                {
+                    Console.WriteLine("Секретное сообщение");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль не верный");
+                    Console.WriteLine("У вас осталось попыток " + (triesCount - (i + additionalAttempt)));
+                }
+                
+            }
+        }
+    }
+}
