3effca677a50f114b955c5411ddef9adfab55837
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-06 20:35:09 +0300

    Add files via upload

---
+++ DegreeOfTwo.v2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerbound = 0;
+            int upperbound = 101;
+            int numberTwo = 2;
+            int resultOfNumber = 1;
+            int number;
+            int degreeCounter = 0;
+            int additionalNumber = 1;
+
+
+            Random ranodom = new Random();
+            number = ranodom.Next(lowerbound,upperbound);
+
+            while (number >= resultOfNumber)
+            {
+                resultOfNumber *= numberTwo;
+                degreeCounter ++;
+            }
+
+            Console.WriteLine($"Число - {number}, степень - {degreeCounter}, число в степени {resultOfNumber}");
+            
+            Console.ReadKey();
+        }
+    }
+}
