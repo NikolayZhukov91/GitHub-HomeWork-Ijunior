14b2f032588a02dd7f50b7d5e1706598524be642
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-06 16:41:05 +0300

    Update and rename MultiplesOfNumbers.v2.cs to MultiplesOfNumbers.v3.cs

--- MultiplesOfNumbers.v3.cs
+++ MultiplesOfNumbers.v3.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MultiplesOfNumbers
 {
@@ -8,22 +8,24 @@ namespace MultiplesOfNumbers
         {
             int lowerBound = 10;
             int upperBound = 25;
+            int addNumber =  1;
             int conditionalMinimumNumber = 50;
             int conditionalMaximumNumber = 150;
             int multiplesOfNumbers = 0;
             
             Random random = new Random();
-            int number = random.Next(lowerBound, upperBound);
+            int number = random.Next(lowerBound, upperBound+addNumber);
 
             for (int i = 0; i <= conditionalMaximumNumber; i += number)
             {
-                if (i >= conditionalMinimumNumber && i <= conditionalMaximumNumber) 
+                if (i >= conditionalMinimumNumber) 
                 {
-                    multiplesOfNumbers++;
-                    Console.WriteLine(multiplesOfNumbers);
-                    Console.WriteLine(i);
+                multiplesOfNumbers++;
+                Console.WriteLine(multiplesOfNumbers);
+                Console.WriteLine(i);
                 }
             }
+
             Console.WriteLine($"Количество чисел от {conditionalMinimumNumber} до {conditionalMaximumNumber}, которые кратны {number} равно {multiplesOfNumbers}.");
             Console.ReadKey();
         }
