8ce4bcec5ad771d00dad68bf084ba26d4d6a6ca0
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-15 09:28:51 +0300

    Add files via upload

---
+++ LocalMaxima.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_maxima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = new int[30];
+            Random random = new Random();
+            int lowerBound = 0;
+            int upperBound = 101;
+            int number = 1;
+            int forFistIndex = 0;
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < elements.Length; i++)
+            {
+                elements[i] = random.Next(lowerBound, upperBound);
+                Console.Write(elements[i] + " ");
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Локальные максимумы:");
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (i == forFistIndex)
+                {
+                    if (elements[i] > elements[i + number])
+                    {
+                        Console.Write(elements[i] + " ");
+                    }
+                }
+
+                else if (i == (elements.Length - number))
+                {
+                    if (elements[i - number] < elements[i])
+                    {
+                        Console.Write(elements[i] + " ");
+                    }
+                }
+
+                else 
+                {    
+                    if (elements[i - number] < elements[i] && elements[i] > elements[i + number])
+                    {
+                        Console.Write(elements[i] + " ");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }   
+    }
+}
