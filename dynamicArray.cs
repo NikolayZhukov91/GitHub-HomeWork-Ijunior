94a13b3cef051fce35aa71c1b12ae96eaf9aa0a7
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-17 09:04:06 +0300

    Add files via upload

---
+++ dynamicArray.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CommandSum = int.MinValue;
+            const int CommandExit = int.MaxValue;
+            bool isWork = true;
+            string userInput;
+            int action;
+            int sum = 0;
+            int[] dinamicArray= new int [0];
+            int[] tempDinamicArray;
+
+            while (isWork)
+            {
+                Console.SetCursorPosition(0, 5);
+                Console.WriteLine("Введите ваше число, или команды 'sum' или 'exit'");
+                userInput = Console.ReadLine();
+                if (userInput == "sum")
+                {
+                    action = CommandSum;
+                }
+                else if (userInput == "exit")
+                {
+                    action = CommandExit;
+                }
+                else 
+                { 
+                    action = Convert.ToInt32(userInput);
+                }
+
+                switch (action)
+                {
+                    case CommandSum:
+                        
+                        for (int i = 0; i < dinamicArray.Length; i++)
+                        {
+                            sum += dinamicArray[i];
+                        }
+                        Console.SetCursorPosition(0, 0);
+                        Console.WriteLine($"{sum} - сумма всех чисел");
+                        break;
+                    case CommandExit:
+                        isWork = false;
+                        break;
+                    default:
+                        tempDinamicArray = new int[dinamicArray.Length + 1];
+                        
+                        for (int i = 0; i < dinamicArray.Length; i++)
+                        {
+                            tempDinamicArray[i] = dinamicArray[i];
+                        }
+                        tempDinamicArray[tempDinamicArray.Length - 1] = action;
+;                       dinamicArray = tempDinamicArray;
+                        Console.SetCursorPosition(0, 1);
+
+                        for (int i = 0;i < dinamicArray.Length; i++)
+                        {
+                            Console.Write(tempDinamicArray[i] + " ");
+                        }
+                        break;
+                }
+                
+            }
+            
+        }
+    }
+}
