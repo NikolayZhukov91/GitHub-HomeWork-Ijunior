31e82179a0ac56a346305fd060ed5d4f9872ed97
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-10-21 13:55:06 +0300

    Add files via upload

---
+++ dynamicArray.v2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Dynamic_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            string userInput;
+            int action;
+            int sum = 0;
+            int[] dinamicArray = new int[0];
+            int[] tempDinamicArray;
+
+            while (isWork)
+            {
+                Console.SetCursorPosition(0, 5);
+                Console.WriteLine("Введите ваше число, или команды 'sum' или 'exit'");
+                userInput = Console.ReadLine();
+                if (userInput == "sum")
+                {
+                    for (int i = 0; i < dinamicArray.Length; i++)
+                    {
+                        sum += dinamicArray[i];
+                    }
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine($"{sum} - сумма всех чисел");
+                }
+                else if (userInput == "exit")
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    action = Convert.ToInt32(userInput);
+                    tempDinamicArray = new int[dinamicArray.Length + 1];
+
+                    for (int i = 0; i < dinamicArray.Length; i++)
+                    {
+                        tempDinamicArray[i] = dinamicArray[i];
+                    }
+                    tempDinamicArray[tempDinamicArray.Length - 1] = action;
+                    dinamicArray = tempDinamicArray;
+                    Console.SetCursorPosition(0, 1);
+                }
+                
+                for (int i = 0; i < dinamicArray.Length; i++)
+                    {
+                        Console.Write(dinamicArray[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+
