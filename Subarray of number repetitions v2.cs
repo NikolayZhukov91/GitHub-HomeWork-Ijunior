4112a8b25736abf961a8bf9a26e84bd5de424b5e
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-12-03 10:59:34 +0300

    Add files via upload

---
+++ Subarray of number repetitions v2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int longestCount = 1;
+        int currentCount = 1;
+        
+        int[] numbers = new int[30];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 11);
+            Console.Write(numbers[i] + " ");
+        }
+        Console.WriteLine();
+
+        int mostFrequentNumber = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] == numbers[i - 1])
+            {
+                currentCount++;
+
+                if (currentCount > longestCount)
+                {
+                    longestCount = currentCount;
+                    mostFrequentNumber = numbers[i];
+                }
+            }
+            else
+            {
+                if (currentCount > longestCount)
+                {
+                    longestCount = currentCount;
+                    mostFrequentNumber = numbers[i - 1];
+                }
+                currentCount = 1;             
+            }
+        }
+
+        Console.WriteLine($"Самое длинное повторение: число {mostFrequentNumber}, количество повторений {longestCount}");
+        Console.ReadKey();
+    }
+}
