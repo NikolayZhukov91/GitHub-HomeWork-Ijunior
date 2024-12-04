9bdc0253cf56f80e4cd05abc1b8960e3af8a74ef
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-12-04 12:48:50 +0300

    Add files via upload

---
+++ Split v2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char separator = ' ';
+            string text = "Привет, я выполняю домашние задания в IJunior";
+            string[] word = text.Split(separator);
+
+            foreach (string subText in word)
+            {
+                Console.WriteLine($"Substring: {subText}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
