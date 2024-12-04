0d8a3945841bd8e7f97709831867dc1a458d9d70
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-12-04 16:20:37 +0300

    Add files via upload

---
+++ ParenthesisExpression.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parenthesis_expression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] parenthesisExpression = new string[] { "(", "(", "(", "(", ")", ")", ")", ")", };
+            int numberRightBrackets = 0;
+            int numberLeftBrackets = 0;
+            int currentDepth = 0;
+
+            for (int i = 0; i < parenthesisExpression.Length; i++)
+            {
+                if (parenthesisExpression[i] == ")")
+                {
+                    numberRightBrackets ++;
+                }
+                else
+                {
+                    numberLeftBrackets ++;
+                }
+            }
+
+            currentDepth += numberLeftBrackets;
+
+            if (numberRightBrackets == numberLeftBrackets)
+            {
+                Console.WriteLine($"Строка корректная. Глубина = {currentDepth}.");
+            }
+            else
+            {
+                Console.WriteLine("Строка не корректная");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
