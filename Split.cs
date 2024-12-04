3b66442232ddb1c103dcbcb8655b6866e89f59da
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-12-04 11:48:41 +0300

    Update Split.cs

--- Split.cs
+++ Split.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Split
 {
@@ -17,6 +13,7 @@ namespace Split
             {
                 Console.WriteLine($"Substring: {subText}");
             }
+            
             Console.ReadKey();
         }
     }
