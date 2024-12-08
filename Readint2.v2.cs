c50d543ac931ec2e091572f8a870bf67cecd0a4f
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-12-08 13:02:21 +0300

    Add files via upload

---
+++ Readint2.v2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReadInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int result = ReadInt();
+            Console.WriteLine($"Введенное число: {result}");
+            Console.ReadKey();
+        }
+
+        static int ReadInt()
+        {
+            string consoleInput = "text";
+            int result;
+
+            
+            while (false == int.TryParse(consoleInput, out result))
+            {
+                Console.Write("Пожалуйста, введите целое число: ");
+                consoleInput = Console.ReadLine();
+            }
+
+            return result;
+        }
+    }
+}
