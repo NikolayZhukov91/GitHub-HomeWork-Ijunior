a6c1efe53a90d3873dd501ed1fd0e87a5143982d
NikolayZhukov91 <55086194+NikolayZhukov91@users.noreply.github.com>
2024-12-09 08:46:02 +0300

    Add files via upload

---
+++ UIElement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int healthPercent = 5, maxHealth = 10;
+            int manaPercent = 7, maxMana = 10;
+
+            while (true)
+            {
+                DrawBar(healthPercent, maxHealth, ConsoleColor.Green, 0, '#');
+                DrawBar(manaPercent, maxMana, ConsoleColor.Blue, 1);
+
+                Console.SetCursorPosition(0, 5);
+                Console.WriteLine("Ввдите процент на которое изменится жизни:");
+                healthPercent += Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ввдите процент на которое изменится мана:");
+                manaPercent += Convert.ToInt32(Console.ReadLine());
+                Console.ReadKey();
+                Console.Clear();
+            }
+            
+
+        }
+
+        static void DrawBar(int valuePercent, int maxValue, ConsoleColor color, int position, char symbol = '|')
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+
+            string bar = "";
+
+            for (int i = 0; i < (valuePercent/maxValue); i++)
+            {
+                bar += symbol;
+            }
+
+            Console.SetCursorPosition(0, position);
+            Console.Write("[");
+            Console.BackgroundColor = color;
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+
+            bar = "";
+
+            for (int i = (valuePercent / maxValue); i < maxValue; i++)
+            {
+                bar += " ";
+            }
+
+            Console.Write(bar + ']');
+        }
+    }
+}
